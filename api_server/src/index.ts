595aa989080e3a610f7b466f25a7198bf9640a82
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-01-24 11:16:45 +0400

    Set up structure/boilerplate

---
+++ api_server/src/index.ts
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/repeat', (req: any, res: any) => {
+
+});
+
+const port: number = 4090;
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+module.exports = server;
---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* Routes to automatically redirect users */}
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
